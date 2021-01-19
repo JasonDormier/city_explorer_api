0aab551dc9e08f605dbc780a6cada96f10f16d79
JasonDormier <jmdormier@gmail.com>
2021-01-18 17:06:28 -0800

    updating server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 /* ----------------- creating the server ----------------- */
 const express = require('express');
 const cors = require('cors');
+const { request } = require('express');
 require('dotenv').config();
 
 /* ----------------- Setup Application Server ----------------- */
@@ -15,8 +16,38 @@ const PORT = process.env.PORT || 3111;
 /* ----------------- Routes ----------------- */
 
 
+app.get('/', (req, res) => {
+  res.send('you made it home. here is change');
+});
 
+app.get('/location', (req, res) => {
+  if (req.query.city === '') {
+    res.status(500).send('No Data My Bad.');
+    return;
+  }
+  const locationData = require('./data/location.json');
+  const locationDataJson = locationData[0];
+  console.log('req.query', req.query);
 
+  const searchLocation = req.query.city;
+
+  const newLocation = new Location(
+    searchLocation,
+    locationDataJson.display_name,
+    locationDataJson.lat,
+    locationDataJson.lon
+  );
+  res.send(newLocation);
+});
+
+/* ----------------- Start the server ----------------- */
+
+function Location(search_query, formatted_query, latitude, longitude) {
+  this.search_query = search_query;
+  this.formatted_query = formatted_query;
+  this.longitude = longitude;
+  this.latitude = latitude;
+}
 
 
 
