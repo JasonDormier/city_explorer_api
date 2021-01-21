6be7109f3ffb79909cfbf57950c2dad73a2ccbb0
JasonDormier <jmdormier@gmail.com>
2021-01-20 18:41:36 -0800

    added sql to server

--- server.js
+++ server.js
@@ -3,17 +3,21 @@
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg');
 
-require('dotenv').config();
+require('dotenv').config();//allows me to read the .env file
 
 /* ----------------- Setup Application Server ----------------- */
 
 const app = express();
 app.use(cors());
 
+const DATABASE_URL = process.env.DATABASE_URL;//add DATABASE_URL to .env
+const client = new pg.Client(DATABASE_URL);
+client.on('error', error => console.log(error));
+
 /* ----------------- Global Varibles ----------------- */
 
-const locationIqKey = process.env.locationIqKey;
 const weatherApiKey = process.env.weatherApiKey;
 const parksApiKey = process.env.parksApiKey;
 const PORT = process.env.PORT || 3111;
@@ -25,41 +29,59 @@ app.get('/', (req, res) => {
 });
 
 app.get('/location', (req, res) => {
-  if (req.query.city === '') {
-    res.status(500).send('No Data My Bad.');
-    return;
-  }
-
   const searchLocation = req.query.city;
-  const locationDataUrl = `https://us1.locationiq.com/v1/search.php?key=${locationIqKey}&q=${searchLocation}&format=json`;
-  superagent.get(locationDataUrl).then(locationDataReturn => {
+  const locationIqKey = process.env.locationIqKey;
 
-    const locationData = locationDataReturn.body[0];
-    const newLocation = new Location(
-      searchLocation,
-      locationData.display_name,
-      locationData.lat,
-      locationData.lon
-    );
-    res.send(newLocation);
-  })
-    .catch(error => {
-      res.status(500).send('locationiq failed');
-      console.log(error.message);
-    });
+  const sqlQuery = 'SELECT * FROM place WHERE search_query=$1';
+  const sqlArray = [searchLocation];
+
+  client.query(sqlQuery, sqlArray).then(result => {
+    console.log(result.rows);
+
+    if (result.rows.length === 0) {
+
+      if (req.query.city === '') {
+        res.status(500).send('No Data My Bad.');
+        return;
+      }
+
+      const locationDataUrl = `https://us1.locationiq.com/v1/search.php?key=${locationIqKey}&q=${searchLocation}&format=json`;
+      superagent.get(locationDataUrl).then(locationDataReturn => {
+
+        const locationData = locationDataReturn.body[0];
+
+        const newLocation = new Location(
+          searchLocation,
+          locationData.display_name,
+          locationData.lat,
+          locationData.lon
+        );
+
+        const sqlQuery = 'INSERT INTO place (search_query, formatted_query, latitude, longitude) VALUES($1, $2, $3, $4)';
+        const sqlArray = [newLocation.search_query, newLocation.formatted_query, newLocation.latitude, newLocation.longitude];
+
+        client.query(sqlQuery, sqlArray);
+
+        res.send(newLocation);
+      })
+        .catch(error => {
+          res.status(500).send('locationiq failed');
+          console.log(error.message);
+        });
+    }
+  });
+
 });
 
 app.get('/weather', (req, res) => {
   const searchLatitude = req.query.latitude;
   const searchLongitude = req.query.longitude;
-  const weatherDataUrl = `https://api.weatherbit.io/v2.0/forecast/hourly?lat=${searchLatitude}&lon=${searchLongitude}&key=${weatherApiKey}&hours=48&units=I`;
 
+  const weatherDataUrl = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${searchLatitude}&lon=${searchLongitude}&key=${weatherApiKey}&days=8&units=I`;
   superagent.get(weatherDataUrl).then(weatherDataReturn => {
-    console.log(weatherDataReturn.body.data);
-    const weatherData = weatherDataReturn.body.data.map(weatherObj => {
-      const newWeatherObj = new Weather(weatherObj);
-      return newWeatherObj;
-    });
+
+    const weatherData = weatherDataReturn.body.data.map(weatherObj => new Weather(weatherObj));
+
     res.send(weatherData);
   })
     .catch(error => {
@@ -73,10 +95,8 @@ app.get('/parks', (req, res) => {
   const parkDataUrl = `https://developer.nps.gov/api/v1/parks?q=${searchLocation}&api_key=${parksApiKey}`;
 
   superagent.get(parkDataUrl).then(parkDataReturn => {
-    const parkData = parkDataReturn.body.data.map(parkObj => {
-      const newparkObj = new Parks(parkObj);
-      return newparkObj;
-    });
+
+    const parkData = parkDataReturn.body.data.map(parkObj => new Parks(parkObj));
     res.send(parkData);
   })
     .catch(error => {
@@ -84,6 +104,7 @@ app.get('/parks', (req, res) => {
       console.log(error.message);
     });
 });
+
 /* ----------------- Helper Functions ----------------- */
 
 function Location(search_query, formatted_query, latitude, longitude) {
@@ -94,8 +115,8 @@ function Location(search_query, formatted_query, latitude, longitude) {
 }
 
 function Weather(weatherObj) {
-  this.forecast = `${weatherObj.weather.description}, ${weatherObj.temp}ºF`;
-  this.time = weatherObj.datetime;
+  this.forecast = `${weatherObj.weather.description}, ${weatherObj.max_temp}ºF`;
+  this.time = weatherObj.valid_date;
 }
 
 function Parks(parksObj) {
@@ -107,5 +128,7 @@ function Parks(parksObj) {
 }
 
 /* ----------------- Start the server ----------------- */
-
-app.listen(PORT, () => console.log(`we are up and running on ${PORT}`));
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`we are up and running on ${PORT}`));
+  });
